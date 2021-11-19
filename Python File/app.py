e03438975a0d8a21f978430f856732c69c9e03c6
mishrayushi2001 <mishrayushi2000@gmail.com>
2021-11-19 13:34:49 +0530

    A

--- Python File/app.py
+++ Python File/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import preprocessor,helper
 
 df = pd.read_csv('athlete_events.csv')
@@ -8,10 +7,17 @@ region_df= pd.read_csv ('noc_regions.csv')
 
 df= preprocessor.preprocess(df,region_df)
 
+st.sidebar.title("Olympics Analysis")
 user_menu = st.sidebar.radio('Select an Option',('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis'))
 
 st.dataframe(df)
 
 if user_menu == 'Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years,country = helper.country_year_list(df)
+    
+    selected_year = st.sidebar.selectbox("Select Year",years)
+    selected_country = st.sidebar.selectbox("Select Country",country)
+    
     medal_tally = helper.medal_tally(df)
     st.dataframe(medal_tally)
